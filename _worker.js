c3f827cc0960bafd6b20f9cbb9661ebc7df0d007
zhsbadsb <101631512+zhsbadsb@users.noreply.github.com>
2024-08-15 14:22:00 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'zhsb.us.kg'
+      url.hostname = 'a1.zhsb.us.kg'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
